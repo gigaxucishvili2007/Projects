a2d3096e0f7117ba558e8d15f8efaefa1a86e85b
gigaxucishvili2007 <158300399+gigaxucishvili2007@users.noreply.github.com>
2025-12-13 11:54:26 +0400

    Add files via upload

---
+++ Books/script.js
@@ -0,0 +1,20 @@
+const track = document.getElementById("tickerTrack");
+const items = [...track.children];
+
+items.forEach((item, index) => {
+    let speed = 80; 
+    let x = track.offsetWidth + (index * 200); 
+
+    function move() {
+        x -= 1; 
+        item.style.transform = `translateX(${x}px)`;
+
+
+        if (x < -item.offsetWidth) {
+            x = track.offsetWidth;
+        }
+        requestAnimationFrame(move);
+    }
+
+    move();
+});
